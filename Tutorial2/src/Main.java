b44e866e96f8b3fc67caca5a1f4e714bebeebe96
sgenGit <jk04140@gmail.com>
2021-02-26 12:02:38 +0900

    2-26

---
+++ Tutorial2/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	
+	final static double PI = 3.141592;
+
+	public static void main(String[] args) {
+		
+		
+
+
+}
---
+++ Tutorial2/src/ex1.java
@@ -0,0 +1,16 @@
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		
+		int a = 200;
+		
+		System.out.println("10진수 : " + a);
+		System.out.format("8 진수 : %o \n", a);
+		System.out.format("16진수 : %x", a);
+		
+		
+		 
+	}
+
+}
